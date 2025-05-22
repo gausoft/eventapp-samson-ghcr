7ef5edef533c4a1b6205c157f07afc8f609605aa
samwin25 <edorhsam@gmail.com>
2025-05-22 11:30:56 +0000

    FRONTEN APPEL API BACKEND

--- TP_EVENT_API/src/routes/eventRoutes.js
+++ TP_EVENT_API/src/routes/eventRoutes.js
@@ -7,7 +7,8 @@ const { validateEventCreation } = require('../middleware/validators');
 router.post('/', validateEventCreation, eventController.createEvent);
 
 // Route pour récupérer tous les événements avec leurs invités
-router.get('/', eventController.getAllEvents);
+router.get('/api/events', eventController.getAllEvents);
+
 
 // Route pour récupérer un événement spécifique avec ses invités
 router.get('/:id', eventController.getEventById);
